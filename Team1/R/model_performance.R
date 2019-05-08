78d2534cfddafabf5b51f5393d3b07ee5002630f
WonderAnn <popovaas@gmail.com>
2019-05-08 17:35:09 +0300

    model performance docs

--- Team1/R/model_performance.R
+++ Team1/R/model_performance.R
@@ -1,4 +1,20 @@
-
+#'Function to model performance
+#'
+#'
+#'
+#'
+#'@param target Basic object like \code{boolean}
+#'
+#'@param predictions Basic object like \code{boolean}
+#'
+#'@keywords model, decision tree, accuracy
+#'
+#'@import rpart, tidyverse
+#'
+#'@examples
+#'model_performance(target, predictions)
+#'
+#'
 
 model_performance <- function(target, predictions) {
   ## Compute the accuracy
