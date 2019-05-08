d9e534ca9c7aa6427ca6daa3a71dd3ba7dab37f1
Vilma <vilma.shehu@raiffeisen.al>
2019-05-08 16:22:01 +0200

    Documentation of Transform file

--- Team1/R/Transform.R
+++ Team1/R/Transform.R
@@ -1,3 +1,22 @@
+#'Function to make neccessary transformation of data
+#'
+#'
+#'
+#'
+#'@param x Basic object like \code{numeric}
+#'@param f Basic object like \code{factor}
+#'
+#'@keywords log , number2factor
+#'
+#'@import
+#'
+#'@examples
+#'logaritmic(bmarketing$age)
+#'logaritmic(bmarketing$duration)
+#'intonum(bmarketing$poutcome)
+#'intonum(bmarketing$euribor3m)
+#'
+
 
 #################Transform numeric variables using the log #################
 
@@ -10,8 +29,8 @@ x<-log(x)
 
 #################Transform factors into numeric variables (and vice versa) as necessary#################
 
-intonum<-factor(x) {
+intonum<-factor(f) {
   
-x <- as.numeric(as.character(x))
+f <- as.numeric(as.character(f))
 
 }
