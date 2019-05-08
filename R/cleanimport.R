14fe2d5a9b7fd4515287bb8186af79f40eac6c8e
patrick sibetz <sibetz.patrick@gmail.com>
2019-05-08 14:28:35 +0200

    cleanimport function

---
+++ R/cleanimport.R
@@ -0,0 +1,9 @@
+# function which imports data set and conducting some data quality checks :
+# * return an error if the target variable contains any missing values (NA’s).
+# * warnings for all other variables which contain NA’s
+# * columns which contain more than 50% NA’s will be removed
+
+cleanimport <- function(){
+  
+  
+}
