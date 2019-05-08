1091652866c3a7fea7e3143ccc066f39ff86d340
Andreas Peterseil <andreas.peterseil@rcb.at>
2019-05-08 14:51:33 +0200

    implemented clean function

--- R/clean.R
+++ R/clean.R
@@ -2,20 +2,38 @@
 #'
 #' \code{sum} returns a clean data set.
 #'
-#' This is a generic function: methods can be defined for it directly
-#' or via the \code{\link{Summary}} group generic. For this to work properly,
-#' the arguments \code{...} should be unnamed, and dispatch is on the
-#' first argument.
-#'
 #' @param data A data.frame
-#' @return A cleaned data.frame. This means:#' 
-#'   \item return an error if the target variable contains any missing values (NA’s).
-#'   \item Give clear warnings for all other variables which contain NA’s.
-#'   \item Remove any columns (and report as warning) which contain more than 50% NA’s.
+#' @return A cleaned data.frame. 
+#'
+#' This means:
+#' 
+#'   - return an error if the target variable contains any missing values (NA’s).
+#'   - Give clear warnings for all other variables which contain NA’s.
+#'   - Remove any columns (and report as warning) which contain more than 50% NA’s.
 #'   
 #' @examples
 #' clean(data)
+#' @export
 
 clean<-function(data){
+  if(sum(is.na(data$y))>0) stop("Target variable y contains missing values (NA's)")
+  for(col in names(data)){
+    
+    if(sum(is.na(data[,col]))>0){
+      
+      if(sum(is.na(data[,col]))/nrow(data) > 0.5){
+        
+        data <- subset(data ,remove = col)
+        warning(paste0("Variable ",col," removed due to more than 50% NA's"))
+       
+      }else{
+        warning(paste0("Variable ",col," contains NA's"))
+      }
+      
+    }
+    
+  }
+  
+  return(data)
   
 }
