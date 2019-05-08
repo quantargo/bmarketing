51eae2b853ac9105f962353266f0d3e078edc1b2
patrick sibetz <sibetz.patrick@gmail.com>
2019-05-08 15:47:16 +0200

    small changes

--- R/transform.R
+++ R/transform.R
@@ -1,9 +1,11 @@
 translog<-function(var){
-  if (is.numeric(var)) var<-log(var)
+    if ( !is.numeric(x) ) stop("Input must be numeric!")
+    if ( any(x<0) ) stop("Input must not be negative!")
+    var <- log(var)
 }
 
 trans<-function(var){
-  if (is.numeric(var)) v<-as.factor(var)
-  if (is.factor(var)) v<-as.numeric(as.character(var))
-  var<-v
+    if (is.numeric(var)) v <- as.factor(var)
+    if (is.factor(var)) v <- as.numeric(as.character(var))
+    var<-v
 }
