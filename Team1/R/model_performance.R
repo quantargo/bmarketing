f59fc370f0e9851a808524b4492ce08236d463c9
PREMTON HYSENI <premtonhyseni@gmail.com>
2019-05-08 16:17:03 +0200

    update perfomance

--- Team1/R/model_performance.R
+++ Team1/R/model_performance.R
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
+
 model_performance <- function(target, predictions) {
-=======
-model_performance <- function(dataset, target, predictions) {
->>>>>>> 88f8363ad0c8b6f1e6ff90b7ce5f480533d994ba
   ## Compute the accuracy
   mean(target == predictions)
   # Lets look at the confusion matrix
