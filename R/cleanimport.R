501cbb13d39dab2c5660a36047e502b742a70e1e
pat sibi <32675357+sibidev@users.noreply.github.com>
2019-05-08 14:36:57 +0200

    Delete cleanimport.R

--- R/cleanimport.R
+++
@@ -1,9 +0,0 @@
-# function which imports data set and conducting some data quality checks :
-# * return an error if the target variable contains any missing values (NA’s).
-# * warnings for all other variables which contain NA’s
-# * columns which contain more than 50% NA’s will be removed
-
-cleanimport <- function(){
-  
-  
-}
