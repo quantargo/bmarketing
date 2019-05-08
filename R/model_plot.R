245d8037b373c50e75195a489f5f95d8642cd8a2
marinapopovic11 <50150624+marinapopovic11@users.noreply.github.com>
2019-05-08 15:59:46 +0200

    added roxygen

--- R/model_plot.R
+++ R/model_plot.R
@@ -1,3 +1,11 @@
+#' Plot the tree and respective nodes
+#'
+#' @param dt_model decisin tree model
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model_plot <- function(dt_model) {
   rpart.plot(dt_model)
 
