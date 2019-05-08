d730b5a36df90410fb821f259cfd151710d5ab82
[llaslopova] <[llaslopova@gmail.com]>
2019-05-08 14:17:58 +0000

    added documentation for model functions

--- R/dtree.R
+++ R/dtree.R
@@ -1,22 +1,62 @@
-dtree <- function(udata) {  
-    dt_model<-rpart(y ~ ., data = udata)
+#' dtree
+#' @description fits the decision tree model based on
+#' @param target target variable from the input data. 
+#' @param udata Input data for the model.
+#' @return A decision tree model.
+#' @examples
+#' dtree(mytarget,mydata)
+
+dtree <- function(target,udata) {  
+    dt_model<-rpart(target ~ ., data = udata)
     dt_model
 }
+
+
+#' dtreesummary
+#' @description returns the summary of the chosen model
+#' @param dt_model model that we want to summarize
+#' @return a summary. :)
+#' @examples
+#' dtreesummary(mymodel)
 
 dtreesummary<-function(dt_model){
   summary(dt_model)
 }
 
+#' dtreeplot
+#' @description returns the plot of the chosen decision tree model
+#' @param dt_model model that we want to plot
+#' @return a plot. :)
+#' @examples
+#' dtreeplot(mymodel)
+
 dtreeplot<-function(dt_model) {
   rpart.plot(dt_model)
 }
 
-dtreepredict<-function(fitdata,predictdata){
-  dt_model <- dtree(fitdata)
+#' dtreepredict
+#' @description returns the prediction 
+#' @param dt_model model that we want to use to generate our predictions
+#' @param predictdata data that we want to score
+#' @return predictions
+#' @examples
+#' dtreepredict(mymodel,mydata)
+#' 
+dtreepredict<-function(dt_model,predictdata){
+
   predictions <- predict(dt_model, predictdata, type = "class")
   predictions
 }
 
+
+#' dtreeperf
+#' @description checks model accuracy
+#' @param target actual target in our data
+#' @param predictions predicted target
+#' @return accuracy
+#' @examples
+#' dtreepredict(mytarget,mypredictions)
+#' 
 dtreeperf<-function(target,predictions){
   accuracy<-mean(target == predictions)
   accuracy
