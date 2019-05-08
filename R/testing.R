321aa9c7bc3e524197b3a74aac4f848d5bf10e61
Predrag Maletic <djape99@gmail.com>
2019-05-08 14:42:29 +0000

    testing all functuions on one place

---
+++ R/testing.R
@@ -0,0 +1,17 @@
+library(tree3)
+
+data("bmarketing")
+bmarketing
+
+t2 <- clean(bmarketing)
+t3 <- transform(t2)
+
+dt_model <- tree3::model(t3)
+
+pred <- predictions(dt_model,t3)
+
+model_accuracy(t3$y,pred)
+
+
+
+
