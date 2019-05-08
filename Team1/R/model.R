99675a94ec45da4bfa4fc3bc1bffdb1c908f3940
WonderAnn <popovaas@gmail.com>
2019-05-08 17:28:52 +0300

    Docs for Model.R

--- Team1/R/model.R
+++ Team1/R/model.R
@@ -1,3 +1,21 @@
+#'Function to histogram
+#'
+#'
+#'
+#'
+#'@param dataset Basic object like \code{numeric, char, factor,boolean, NULL}
+#'
+#'@param target Basic object like \code{boolean}
+#'
+#'@keywords model, decision tree,
+#'
+#'@import rpart.plot, rpart, tidyverse
+#'
+#'@examples
+#'model(bmarketing, bmarketing$y)
+#'
+#'
+
 # model function with 2 arguments: target and dataset 
 model <- function(dataset, target) {
   rpart(target ~ ., data = dataset) 
