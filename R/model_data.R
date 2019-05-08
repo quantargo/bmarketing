026610db6cfbc663159605dc672613a4694b0ea4
marinapopovic11 <50150624+marinapopovic11@users.noreply.github.com>
2019-05-08 16:07:40 +0200

    added roxygen

--- R/model_data.R
+++ R/model_data.R
@@ -1,3 +1,12 @@
+#' Returns the predicted classes from the decision tree
+#'
+#' @param dataset Data frame on which we want to make a model
+#' @param target_var Dependent variable 
+#'
+#' @return return decision tree
+#' @export
+#'
+#' @examples
 model_data <- function(dataset, target_var){
   
   dt_model<- rpart(y ~ ., data = dataset )
