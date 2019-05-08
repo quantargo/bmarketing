dee93ebc71d24d3d40e76e263585cba507e8f7f6
WonderAnn <popovaas@gmail.com>
2019-05-08 17:32:45 +0300

    Docs prediction

--- Team1/R/predict.R
+++ Team1/R/predict.R
@@ -1,3 +1,21 @@
+#'Function to model predict
+#'
+#'
+#'
+#'
+#'@param dataset Basic object like \code{numeric, char, factor,boolean, NULL}
+#'
+#'@param dt_model Basic object like \code{object}
+#'
+#'@keywords model, decision tree, prediction
+#'
+#'@import rpart, tidyverse
+#'
+#'@examples
+#'model_pred(bmarketing, model)
+#'
+#'
+
 #prediction function
 model_pred <- function(dataset, dt_model) {
   predict(dt_model, dataset, type = "class") 
