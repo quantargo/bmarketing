57d03d03699318f94c475d842535731a67e42b9c
Michael Mifek <mifek@me.com>
2019-05-08 15:18:29 +0200

    Added clean_data test

--- tests/testthat/test_clean_data.R
+++ tests/testthat/test_clean_data.R
@@ -1,9 +1,12 @@
 context("clean_data")
 
-test_import <- function() {
-  ## create test data for test
-  ##expect_error()    check if error is thrown in the case of missing target_var
-  ##expect_warning()  check if warnings are thrown
-  ##expect_equal()    check ncol of result
-  expect_equal(1, 1)
+test_that("import and data cleansing works", {
+  ## expected no of rows received
+  expect_equal(nrow(clean_data("data/bmarketing.csv", target_var = "y")), 4120)
+  ## check if error is thrown in the case of missing target_var
+  expect_error(clean_data("data/bmarketing.csv", target_var = "unknown"))    
+  ## check if warnings are thrown and columns are excluded
+  expect_warning(clean_data("data/bmarketing_with_nas.csv", target_var = "y"))  
+  expect_equal(ncol(clean_data("data/bmarketing_with_nas.csv", target_var = "y"))  , 5)
 }
+)
