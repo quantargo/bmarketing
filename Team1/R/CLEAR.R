3f388c7c6167e3b0b5b85fb49eeeb77cf9fda375
JuliaPetranova <petranova.julia@gmail.com>
2019-05-08 13:50:40 +0000

    adding new function for clear

--- Team1/R/CLEAR.R
+++ Team1/R/CLEAR.R
@@ -2,18 +2,21 @@
 # If newdata has same number of observation that implies no NA value present
 # is.na(bmarketing)
 
-clear1 <- function(x) {
-  newdata <- na.omit(x)
-  nrow(newdata)==nrow(x)
+clear1 <- function(data) {
+  newdata <- na.omit(data)
+  if (nrow(newdata)==nrow(data)) {
+    print("All okay: No Missing Value found")
+  } 
+  else{
+    print("Error: Missing Values found")
+  }
 }
 
-x <- bmarketing
-clear1(x)
 
 #A deep check for a particular column let say age
 
-clear2 <- function(w) {
-  if(length(which(is.na(w)==TRUE)>0)){
+clear2 <- function(data,target) {
+  if(length(which(is.na(data$target)==TRUE)>0)){
     print("Missing Value found in the specified column")
   } 
   else{
@@ -21,5 +24,13 @@ clear2 <- function(w) {
   }
 }
 
-w <- bmarketing$y
-clear2(w)
+ 
+
+
+if(length(which(is.na(bmarketing$y)==TRUE)>length(bmarketing$y)/2)){
+  print("Missing Value found in the specified column")
+} else{
+  print("All okay: No Missing Value found in the specified column")
+}
+  
+
