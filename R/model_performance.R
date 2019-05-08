c43e5e8ff0af9f3c52db4ca2d73300c055525557
lyrabe <lyrabeciri@gmail.com>
2019-05-08 14:11:16 +0000

    Added roxygen comment

--- R/model_performance.R
+++ R/model_performance.R
@@ -1,3 +1,11 @@
+#' Calculate the accuracy of the model
+#'
+#' @param data Dataset that we want to predict on 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 model_performance <- function(data) {
   model_performance <- mean(bmarketing$y == predictions)
   model_perfomance()
