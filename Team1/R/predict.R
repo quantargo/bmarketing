184efe7156a8881ddb73954436fc4accecb80bb7
PREMTON HYSENI <premtonhyseni@gmail.com>
2019-05-08 15:55:28 +0200

    prediction model

--- Team1/R/predict.R
+++ Team1/R/predict.R
@@ -1,11 +1,4 @@
-#prediction fundtion
-
-model <- function(dataset, target) {
-  dt_model<- rpart(target ~ ., data = dataset) 
-}
-
-prediction <- function(dataset, target) {predict(rpart(target ~ ., data = dataset), dataset, type = "class")}
-
+#prediction function
 model_pred <- function(dataset, dt_model) {
-  predictions <- predict(dt_model, dataset, type = "class") 
+  predict(dt_model, dataset, type = "class") 
 }
