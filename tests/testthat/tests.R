1e18a3cdcaaa206141de0f159814ac8d5d914822
patrick sibetz <sibetz.patrick@gmail.com>
2019-05-09 12:12:24 +0200

    fixed tests for checkNA

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -2,10 +2,7 @@ library(testthat)
 
 testthat::test_that("test checkNA",{
   
-  # --- the following tests do not work ... yet
-  #expect_equal( checkNA(matrix( c(1:9),3,3) ) , "No empty records found in the dataframe" ) 
-  #expect_equal( checkNA(matrix( c(1:8, NA),3,3) ) , "There are   1 columns having NAsNULL")
+  expect_equal( checkNA(matrix( c(1:9),3,3) ) , "No empty records found in the dataframe" ) 
+  expect_equal( checkNA(matrix( c(1:8, NA),3,3) ) , "There are 1 columns having NAs")
   
-  expect_equal(1,1)
 })
-
