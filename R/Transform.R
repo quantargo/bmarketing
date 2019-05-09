25506826bcf595878a74a4e92eff12568e6d5194
Balint Komjati <bkomjati@gmail.com>
2019-05-09 09:01:32 +0200

    Transform funciton comments translated to EN so Team4 colleagues can also read it. Have fun ;)

--- R/Transform.R
+++ R/Transform.R
@@ -1,7 +1,10 @@
+
+#todos
+#function should give warning is a variable was log transformed
+#log transformation should be optional
+
 transform <- function(input) {
   output <- as.data.frame(lapply(input, function(x) {
-    
-    #if(!is.numeric(x)) x <- as.numeric(as.character(x))
     if(is.numeric(x) && min(x)>0) {
       xshapiro <- shapiro.test(x)
       if(xshapiro$p.value <.05) { 
@@ -10,8 +13,7 @@ transform <- function(input) {
     x  
   }))
   output
-  #warning arról hogy mi lett transzformálva
-  #optionálisan legyen csak a logtrafo
+
 }
 
 
