6e74430737eb8151426e95f4e88e9b1988e64d93
Andreas Peterseil <andreas.peterseil@rcb.at>
2019-05-09 14:52:43 +0200

    changed library loading

--- R/model.R
+++ R/model.R
@@ -18,10 +18,8 @@
 #' @export
 
 model <- function(input_data,target_name) {
-  library(rpart)
-  library(rpart.plot)
-  dt_model<- rpart(as.formula(paste(target_name," ~ .")), data = input_data)
-  rpart.plot(dt_model)
+  dt_model<- rpart::rpart(as.formula(paste(target_name," ~ .")), data = input_data)
+  rpart.plot::rpart.plot(dt_model)
   return (dt_model)
 }
 
