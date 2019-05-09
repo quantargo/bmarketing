3b57397f8424ddae60d4bc491367ae82d59cfd5b
Michael Mifek <mifek@me.com>
2019-05-09 15:24:34 +0200

    Added test from transform_data function

--- tests/testthat/test_transform_data.R
+++ tests/testthat/test_transform_data.R
@@ -1,1 +1,17 @@
-context("aaa")
+context("Data Transformation")
+
+test_that("imputation works as expected", {
+  expect_equal(sum(is.na(test_data$BALANCE)), 9)
+  expect_equal(sum(is.na(transform_data(test_data, cols_impute = "BALANCE")$BALANCE)), 0)
+  expect_equal(mean(transform_data(test_data, cols_impute = "BALANCE")$BALANCE), mean(test_data$BALANCE, na.rm = TRUE))
+})
+
+test_that("log transform works as expected", {
+  ## Value calculated once manual
+  expect_equal(mean(transform_data(test_data, cols_impute = "AGE", cols_log = "AGE")$AGE), 3.81205)
+})
+
+test_that("normalization works as expected", {
+  ## Value calculated once manual
+  expect_equal(mean(transform_data(test_data, cols_impute = "AGE", cols_normalize = "AGE")$AGE), 0)
+})
