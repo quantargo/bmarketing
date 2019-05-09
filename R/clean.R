084f410fe457700f4ccc51c8d5afa0037196c79b
Zoltan Szebenyi <zoltan.szebenyi@rbinternational.com>
2019-05-09 11:00:42 +0000

    fix issues #5, #8 and #10

--- R/clean.R
+++ R/clean.R
@@ -16,13 +16,13 @@
 clean <- function(x, t) {
   if (is.data.frame(x)==FALSE) {stop("Parameter x must be a dataframe")}
   if (is.na(match(t, names(x)))==TRUE) {stop("Parameter t must be the name (string) of a column in the dataframe")}
-  if (any(is.na(x$t))==TRUE) {stop("The target variabe t contais NA values")}
+  if (any(is.na(x[[t]]))==TRUE) {stop(paste("The target variabe", t, "contais NA values"))}
   if (any(is.na(x[ , -which(colnames(x)==t)]))==TRUE) {warning("Explanatory variables contain NA values")}
   count_na <- sapply(x, function(y) sum(length(which(is.na(y))))/length(y))
   cols_to_remove <- names(count_na[count_na > 0.5])
   if (length(cols_to_remove)==0) {return(x)}
   else {
-    warning(paste("The following columns are removed: ", cols_to_remove))
+    warning(paste("The following columns are removed: ", paste(cols_to_remove,collapse=", ")))
     return(x[,-which(colnames(x)==cols_to_remove)])
     }
 }
