25617ec8220ca6edc6c396738b6c4dae8551c065
marinapopovic11 <50150624+marinapopovic11@users.noreply.github.com>
2019-05-09 12:25:34 +0200

    Added example in roxygen

--- R/model_plot.R
+++ R/model_plot.R
@@ -4,6 +4,7 @@
 #'
 #' @export
 #'
+#' @example You can plot it by invoke function model_plot(dt_model)
 #' @importFrom rpart.plot rpart.plot
 model_plot <- function(dt_model) {
   rpart.plot(dt_model)
