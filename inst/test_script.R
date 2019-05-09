a242ac2ab5f5bfe3bc813bf085e4c6ba4e05e103
Michael Mifek <mifek@me.com>
2019-05-09 11:59:16 +0200

    Added test script

--- inst/test_script.R
+++ inst/test_script.R
@@ -1,7 +1,16 @@
 library(bmarketing)
 
+
 df <- import_data_from_csv("~/CloudStation/Projekte/r_projects/bmarketing2.csv")
 clean_data(df, target_var = "Y")
 
 df[is.na(df$Y), "Y"] <- 0
-clean_data(df, target_var = "Y")
+df <- clean_data(df, target_var = "Y")
+
+df <- transform_data(df, c("AGE", "DURATION"))
+
+target_var <- "Y"
+model_data(df, target_var)
+
+
+
