51408d548e2438a139f31d20adf679a460bf2de5
patrick sibetz <sibetz.patrick@gmail.com>
2019-05-09 15:26:36 +0200

    import glm

--- R/logit.R
+++ R/logit.R
@@ -1,12 +1,14 @@
 #' logit
 #' @description fits the logit model based on
+#' 
 #' @param target target variable from the input data. 
 #' @param udata Input data for the model.
+#' 
 #' @return logit model.
 #' 
+#' @importFrom stats glm
+#' 
 #' @export
-
-
 logit<-function(udata,target){
   glm(target~., data=udata,family = "binomial") 
 }
