c0e1524d5f0335af68b81e9b10b61618f841714b
Michael Mifek <mifek@me.com>
2019-05-09 14:54:38 +0200

    Updated Tests

--- tests/testthat/test_clean_data.R
+++ tests/testthat/test_clean_data.R
@@ -1,18 +1,15 @@
-context("clean_data")
+context("import and clean")
 
-file1 <- "bmarketing.csv"
-file2 <- "bmarketing_with_nas.csv"
+filename <- "testdata.csv"
+data <- import_data_from_csv(filename)
+data[is.na(data$Y), "Y"] <- "no"
 
-test_that("import and data cleansing works", {
-  ## expected no of rows received
-  ##expect_equal(nrow(clean_data(file1, target_var = "y")), 4119)
-  ## check if error is thrown in the case of missing target_var
-  ##expect_error(clean_data(file1, target_var = "unknown"))    
+test_that("import works", {
+  expect_equal(nrow(data), 99)
 })
 
-test_that("handling of NAs works", {
-  ## check if warnings are thrown 
-  ##expect_warning(clean_data(file2, target_var = "y"), regexp = "will be excluded$")  
-  ## check if columns are excluded
-  ##expect_equal(ncol(clean_data(file2, target_var = "y"))  , 18)
+test_that("cleaning works", {
+  ## Check if NA data in target column throws an error
+  expect_error(clean_data(data, target_var = "AGE"), regexp = "Target Var should not include any NAs")
+  expect_warning(clean_data(data, "Y"), regexp = "too many NAs and will be excluded")
 })
